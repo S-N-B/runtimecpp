24b5db7851bfab8a4172d0ede3a54bcec963cd00
coder3840 <37975151+coder3840@users.noreply.github.com>
2018-04-01 15:50:33 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<ctime>//or<time.h>
+int main()
+{
+	clock_t start=clock();
+	for(int i=1;i<=100;i++)
+		std::cout<<i*i<<" ";
+	std::cout<<"\nTotal time="<<(double)(clock()-start)/CLOCKS_PER_SEC<<"s\n";
+	return 0;
+}
+//Also we can add 'time' before any command in terminal to get its execution time
+//So simply run 'time ./a.out' and we will get the total execution time :-)
