0c4d237831dc8e217b9f5b21b12ee847f1dbf315
coder3840 <shyamnbajaj1998@gmail.com>
2018-07-16 22:34:42 +0530

    added comments

--- code.cpp
+++ code.cpp
@@ -6,6 +6,7 @@ int main()
 	for(int i=1;i<=100;i++)
 		std::cout<<i*i<<" ";
 	std::cout<<"\nTotal time="<<(double)(clock()-start)/CLOCKS_PER_SEC<<"s\n";
+	//edit;
 	return 0;
 }
 //Also we can add 'time' before any command in terminal to get its execution time
